8a1fed1996660e0e55ec4de54654691ae9da5642
xbb1973 <xiaobanban1973@163.com>
2021-05-31 01:44:06 +0800

    back track 3.

--- myleetcode/src/LeetCode.java
+++ myleetcode/src/LeetCode.java
@@ -3895,6 +3895,97 @@ class Solution {
 
     // 3. 在矩阵中寻找字符串
     // 79. Word Search (Medium)
+    int[][] Direactionn = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int height;
+    int width;
+    boolean[][] visited;
+    // Code with over time ...
+    public boolean exist(char[][] board, String word) {
+        height = board.length;
+        width = board[0].length;
+        visited = new boolean[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (backTracking(board, i, j, word)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // key point int[][] first parameter is height.
+    private boolean backTracking(char[][] board, int i, int j, String word) {
+        if (word.length() == 0) {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= height || j >= width
+                || board[i][j] != word.charAt(0)
+                || visited[i][j]) {
+            return false;
+        }
+        char c = board[i][j];
+        System.out.println(c);
+        visited[i][j] = true;
+        for (int[] direction : Direactionn) {
+            if (backTracking(board, i + direction[0], j + direction[1], word.substring(1))) {
+                return true;
+            }
+        }
+        visited[i][j] = false;
+        return false;
+    }
+
+    // AC Code
+    private final static int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private int m;
+    private int n;
+    public boolean exist(char[][] board, String word) {
+        if (word == null || word.length() == 0) {
+            return true;
+        }
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+
+        m = board.length;
+        n = board[0].length;
+        boolean[][] hasVisited = new boolean[m][n];
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (backtracking(0, r, c, hasVisited, board, word)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean backtracking(int curLen, int r, int c, boolean[][] visited, final char[][] board, final String word) {
+        if (curLen == word.length()) {
+            return true;
+        }
+        if (r < 0 || r >= m || c < 0 || c >= n
+                || board[r][c] != word.charAt(curLen) || visited[r][c]) {
+
+            return false;
+        }
+
+        visited[r][c] = true;
+
+        for (int[] d : direction) {
+            if (backtracking(curLen + 1, r + d[0], c + d[1], visited, board, word)) {
+                return true;
+            }
+        }
+
+        visited[r][c] = false;
+
+        return false;
+    }
+
 
     // 4. 输出二叉树中所有从根到叶子的路径
     // 257. Binary Tree Paths (Easy)
